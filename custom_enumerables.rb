2062be38803d9cd0c86905cdc932c982e2b69002
meowzie <appenge2@gmail.com>
2022-01-14 21:26:58 +0200

    modified method my_each to yield to blocks for every item even when the item is not taken as a parameter

--- custom_enumerables.rb
+++ custom_enumerables.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
+# The same built-in Enumerable module with added methods
 module Enumerable
   def my_each
-    yield(self)
+    times = length
+    index = 0
+    times.times do
+      yield(self[index])
+      index += 1
+    end
     self
   end
 end
