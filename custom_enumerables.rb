8c3cc9fe7b3f65279dae81d173a472dc9a116c28
meowzie <appenge2@gmail.com>
2022-01-14 23:02:07 +0200

    Added Enumerable#my_select

--- custom_enumerables.rb
+++ custom_enumerables.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'pry-byebug'
+
 # The same built-in Enumerable module with added methods
 module Enumerable
   def my_each
@@ -16,9 +18,15 @@ module Enumerable
     times = length
     index = 0
     times.times do
-      yield(self[index], index)
+      instance_of?(Array) ? yield(self[index], index) : yield(keys[index], values[index])
       index += 1
     end
     self
+  end
+
+  def my_select(&condition)
+    truthies = []
+    my_each { |item| truthies.push(item) if condition.call(item) }
+    truthies
   end
 end
