f7dc5b05d8edbf038533462266010751507f197b
meowzie <appenge2@gmail.com>
2022-01-15 17:49:45 +0200

    Added Enumerable#my_map and Enumerable#my_inject

--- custom_enumerables.rb
+++ custom_enumerables.rb
@@ -59,4 +59,42 @@ module Enumerable
       length
     end
   end
+
+  def my_map(&block)
+    if instance_of?(Array)
+      result = []
+      my_each { |item| result.push(block.call(item)) }
+    else
+      result = {}
+      my_each { |key, value| result[key] = block.call(key, value) }
+    end
+    result
+  end
+
+  # does not work on hashes
+  def my_inject(*args, &block)
+    if args.length == 2
+      initial_operand = [args[0]]
+      operator = args[1]
+    elsif args.length == 1
+      case block_given?
+      when false
+        initial_operand = []
+        operator = args[0]
+      when true
+        initial_operand = [args[0]]
+      end
+    elsif args.empty?
+      initial_operand = []
+    end
+    result = initial_operand.empty? ? first : initial_operand.my_inject(operator)
+    if block_given?
+      to_a.my_each { |item| result == item ? next : result = block.call(result, item) }
+    else
+      to_a.my_each { |item| result == item ? next : result = result.method(operator).call(item) }
+    end
+    result
+  end
 end
+
+puts((1..5).my_inject(10) { |acc, num| acc * num })
